2aad925f40a06f2524d1d1b4ffdd8905aa366a39
unknown <pmurph0305@gmail.com>
2019-09-12 13:20:02 -0400

    Part 1 gatsby building blocks & Part 2 introduction to styling

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Header from "../components/header"
+
+export default () => (
+  <div style={{ color: `teal` }}>
+    <Header headerText="About Gatsby" />
+    <Header headerText="Its cool." />
+    <p>Such wow. Very React.</p>
+  </div>
+)
