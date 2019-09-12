2a78a1b5794be7b4707d78913b6f2c1142828910
unknown <pmurph0305@gmail.com>
2019-09-12 16:06:06 -0400

    Header and Footer in layout

---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default () => (
+  <footer style={{ textAlign: `right` }}>
+    <p style={{ fontSize: `0.5em`, margin: `0` }}>Created by FakeName</p>
+    <p style={{ fontSize: `0.5em`, margin: `0` }}>
+      Site built with Gatsby and Typography.js
+    </p>
+  </footer>
+)
---
+++ src/components/navbar.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const ListLink = props => (
+  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+    <Link to={props.to}>{props.children}</Link>
+  </li>
+)
+
+export default () => (
+  <header>
+    <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+      <h3 style={{ display: `inline` }}>MySweetSite</h3>
+    </Link>
+    <ul style={{ listStyle: `none`, float: `right` }}>
+      <ListLink to="/">Home</ListLink>
+      <ListLink to="/about/">About</ListLink>
+      <ListLink to="/contact/">Contact</ListLink>
+    </ul>
+  </header>
+)
