d6c19a5f114a9c82ea895bf4bfea3c4754f61e06
unknown <pmurph0305@gmail.com>
2019-09-13 10:21:43 -0400

    source plugins and rendering queried data

---
+++ src/pages/my-files.js
@@ -0,0 +1,47 @@
+import React from "react"
+import { graphql } from "gatsby"
+import DataLayout from "../components/dataLayout"
+export default ({ data }) => {
+  console.log(data)
+  return (
+    <DataLayout>
+      <div>
+        <h1>Files</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>relativePath</th>
+              <th>prettySize</th>
+              <th>extension</th>
+              <th>birthTime</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.allFile.edges.map(({ node }, index) => (
+              <tr key={"node_" + index}>
+                <td>{node.relativePath}</td>
+                <td>{node.prettySize}</td>
+                <td>{node.extension}</td>
+                <td>{node.birthTime}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </DataLayout>
+  )
+}
+export const query = graphql`
+  query {
+    allFile {
+      edges {
+        node {
+          relativePath
+          prettySize
+          extension
+          birthTime(fromNow: true)
+        }
+      }
+    }
+  }
+`
